6971e036d0fa7d69d9c3f012979b4b24bd4682d3
bwees <brandonwees@gmail.com>
2025-11-25 00:38:16 -0600

    feat: wip apply edits

--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -346,6 +346,26 @@ export const tokenizeForSearch = (text: string): string[] => {
   return tokens;
 };
 
+export function isEditedAsset<O>(qb: SelectQueryBuilder<DB, 'asset', O>, edited: boolean) {
+  return qb.where((eb) =>
+    eb.exists(
+      eb
+        .selectFrom('asset_file')
+        .whereRef('asset_file.assetId', '=', 'asset.id')
+        .where('asset_file.edited', '=', edited),
+    ),
+  );
+}
+
+export function withEdits(eb: ExpressionBuilder<DB, 'asset'>) {
+  return jsonArrayFrom(
+    eb
+      .selectFrom('asset_edit')
+      .select(['asset_edit.action', 'asset_edit.parameters', 'asset_edit.index'])
+      .whereRef('asset_edit.assetId', '=', 'asset.id'),
+  ).as('edits');
+}
+
 const joinDeduplicationPlugin = new DeduplicateJoinsPlugin();
 /** TODO: This should only be used for search-related queries, not as a general purpose query builder */
 
