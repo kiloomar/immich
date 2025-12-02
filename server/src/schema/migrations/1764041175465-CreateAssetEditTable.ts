458d0fd17ee14e0ecf81e7bf3ea4554e0c610960
bwees <brandonwees@gmail.com>
2025-12-02 12:08:14 -0600

    fix: primary key on asset edit table

--- server/src/schema/migrations/1764041175465-CreateAssetEditTable.ts
+++ server/src/schema/migrations/1764041175465-CreateAssetEditTable.ts
@@ -3,12 +3,18 @@ import { Kysely, sql } from 'kysely';
 export async function up(db: Kysely<any>): Promise<void> {
   await sql`
     CREATE TABLE "asset_edit" (
+      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
       "assetId" uuid NOT NULL,
       "action" varchar NOT NULL,
-      "parameters" jsonb NOT NULL,
-      CONSTRAINT "asset_edit_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "asset"("id") ON DELETE CASCADE ON UPDATE CASCADE
+      "parameters" jsonb NOT NULL
     );
   `.execute(db);
+
+  await sql`ALTER TABLE "asset_edit" ADD CONSTRAINT "asset_edit_pkey" PRIMARY KEY ("id");`.execute(db);
+  await sql`ALTER TABLE "asset_edit" ADD CONSTRAINT "asset_edit_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "asset" ("id") ON UPDATE CASCADE ON DELETE CASCADE;`.execute(
+    db,
+  );
+  await sql`CREATE INDEX "asset_edit_assetId_idx" ON "asset_edit" ("assetId")`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
--- server/src/schema/tables/asset-edit.table.ts
+++ server/src/schema/tables/asset-edit.table.ts
@@ -1,8 +1,11 @@
 import { EditAction, EditActionParameter } from 'src/dtos/editing.dto';
 import { AssetTable } from 'src/schema/tables/asset.table';
-import { Column, ForeignKeyColumn } from 'src/sql-tools';
+import { Column, ForeignKeyColumn, Generated, PrimaryGeneratedColumn } from 'src/sql-tools';
 
 export class AssetEditTable<T extends EditAction = EditAction> {
+  @PrimaryGeneratedColumn()
+  id!: Generated<string>;
+
   @ForeignKeyColumn(() => AssetTable, { onDelete: 'CASCADE', onUpdate: 'CASCADE', nullable: false, primary: true })
   assetId!: string;
 
