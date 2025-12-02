3b68d50033de0fa57e3001d71dae83a970bdad02
bwees <brandonwees@gmail.com>
2025-12-02 12:27:50 -0600

    fix: db migrations

--- server/src/schema/migrations/1764458955216-CreateIsVisibleColumns.ts
+++ server/src/schema/migrations/1764458955216-CreateIsVisibleColumns.ts
@@ -1,13 +1,11 @@
 import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  // add the isVisible column with default true
-  await sql`ALTER TABLE "asset_ocr" ADD COLUMN IF NOT EXISTS "isVisible" boolean DEFAULT true;`.execute(db);
-  await sql`ALTER TABLE "asset_face" ADD COLUMN IF NOT EXISTS "isVisible" boolean DEFAULT true;`.execute(db);
+  await sql`ALTER TABLE "asset_ocr" ADD COLUMN "isVisible" boolean NOT NULL DEFAULT TRUE`.execute(db);
+  await sql`ALTER TABLE "asset_face" ADD COLUMN "isVisible" boolean NOT NULL DEFAULT TRUE`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  // drop the isVisible column
-  await sql`ALTER TABLE "asset_ocr" DROP COLUMN IF EXISTS "isVisible";`.execute(db);
-  await sql`ALTER TABLE "asset_face" DROP COLUMN IF EXISTS "isVisible";`.execute(db);
+  await sql`ALTER TABLE "asset_ocr" DROP COLUMN "isVisible";`.execute(db);
+  await sql`ALTER TABLE "asset_face" DROP COLUMN "isVisible";`.execute(db);
 }
